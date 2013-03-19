35276594b96856db8ecd428c45758b2f260a9910
Tom Dietrich <tom.dietrich@gmail.com>
2013-03-19 12:29:16 -0400

    documentation edit

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,5 +1,11 @@
 namespace StringCalculator
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     public static class StringCalculator
     {
         public static int Add(string numbers)
